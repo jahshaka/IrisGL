640a762f3da0fdd8bf1ada17a22985c310ca5897
Nicolas Brown <nclsbrwn@gmail.com>
2019-02-19 18:23:40 -0500

    Remove commented code from skeleton class

--- src/graphics/skeleton.cpp
+++ src/graphics/skeleton.cpp
@@ -15,43 +15,13 @@ BonePtr Skeleton::getBone(QString name)
 
 void Skeleton::applyAnimation(iris::SkeletalAnimationPtr anim, float time)
 {
+	/*
     for (auto i = 0; i < bones.size(); i++) {
         bones[i]->transformMatrix.setToIdentity();
         bones[i]->localMatrix.setToIdentity();
         bones[i]->skinMatrix.setToIdentity();
-    }
-
-	// We dont have the bones in a hierarchy here
-	// so we eval the local transform
-	/*
-	for (auto bone : bones) {
-        if (anim->boneAnimations.contains(bone->name))
-        {
-			auto boneAnim = anim->boneAnimations[bone->name];
-
-            //qDebug()<< boneName <<" verified";
-            auto pos = boneAnim->posKeys->getValueAt(time);
-            auto rot = boneAnim->rotKeys->getValueAt(time);
-            auto scale = boneAnim->scaleKeys->getValueAt(time);
-
-
-            bone->localMatrix.setToIdentity();
-            bone->localMatrix.translate(pos);
-            bone->localMatrix.rotate(rot);
-            bone->localMatrix.scale(scale);
-        }
-        else
-        {
-			// use original transform
-			bone->localMatrix.setToIdentity();
-			bone->localMatrix.translate(bone->pos);
-			bone->localMatrix.rotate(bone->rot);
-			bone->localMatrix.scale(bone->scale);
-        }
-    }
-	*/
-
-	// recursively update the animation for each node
+    }*/
+	// recursively update the animation for each bone
 	std::function<void(SkeletalAnimationPtr anim, iris::BonePtr bone)> animateHierarchy;
 	animateHierarchy = [&animateHierarchy, time](SkeletalAnimationPtr anim, iris::BonePtr bone)
 	{
@@ -90,44 +60,14 @@ void Skeleton::applyAnimation(iris::SkeletalAnimationPtr anim, float time)
 			animateHierarchy(anim, child);
 		}
 	};
-
-	//QMatrix4x4 rootTransform;
-	//rootTransform.setToIdentity();
-	//rootTransform = this->getLocalTransform();
-	//animateHierarchy(animation->getSkeletalAnimation(), this->sharedFromThis(), rootTransform);
-
-    //recursively calculate final transform
-    std::function<void(BonePtr)> calcFinalTransform;
-    calcFinalTransform = [&calcFinalTransform](BonePtr bone)
-    {
-        for (auto child : bone->childBones) {
-            child->transformMatrix = bone->transformMatrix * child->localMatrix;
-            //child->skinMatrix = child->transformMatrix * child->inverseMeshSpacePoseMatrix;
-            //qDebug() << child->transformMatrix;
-            calcFinalTransform(child);
-        }
-    };
-
-    //auto rootBone = bones[0];//its assumed that the first bone is the root bone
-    //auto rootBone = this->getRootBone();
 	
     auto roots = this->getRootBones();
     for (auto rootBone : roots) {
         rootBone->transformMatrix = rootBone->localMatrix;
-        //rootBone->transformMatrix.setToIdentity();
-        //rootBone->skinMatrix = rootBone->transformMatrix * rootBone->inverseMeshSpacePoseMatrix;
-        //calcFinalTransform(rootBone);
 		
 		animateHierarchy(anim, rootBone);
     }
-	
 
-    //assign transforms to list
-	/*
-    for (auto i = 0; i < bones.size(); i++) {
-        boneTransforms[i] = bones[i]->skinMatrix;
-    }
-	*/
 }
 
 // https://github.com/acgessler/open3mod/blob/master/open3mod/SceneAnimator.cs#L338
